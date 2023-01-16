b159ba999273e57a1d1c2be403535feb16a486c1
Galka <hgalala@mail.ru>
2023-01-16 11:03:47 +0700

    Create Home_work folder with Task6

---
+++ Home_work_1_Task6/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+int flag=1;
+string flag_S;
+string number_S;
+int number;
+int result;
+do{
+Console.WriteLine("Введите число:");
+number_S=Console.ReadLine();
+number=Convert.ToInt32(number_S);
+result=number%2;
+
+if (result==0){Console.WriteLine($"{number} -> да");}
+else {Console.WriteLine($"{number} -> нет");}
+
+Console.WriteLine("Введите 1, чтобы продолжить:");
+flag_S=Console.ReadLine();
+flag=Convert.ToInt32(flag_S);
+}while(flag==1);
