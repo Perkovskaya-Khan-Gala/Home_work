e9a0815418c93549d4390a8c406835b522c81720
Galka <hgalala@mail.ru>
2023-01-23 08:51:16 +0700

    Create folder with Task 23

---
+++ Home_work_3_Task23/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 23
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int GetNNumber(string message)
+{
+    int result = 0;
+    Console.WriteLine(message);
+    while (true)
+    {
+        bool y_or_n = int.TryParse(Console.ReadLine(), out result);
+        if (y_or_n&&result>=0)
+            break;
+        else
+            Console.WriteLine("Введено не число или отрицательное число. Попробуйте еще раз");
+    }
+    return result;
+}
+
+void ShowCubeTable(int number)
+{
+    
+    int i=1;
+    Console.Write($"{number} -> ");
+    while(i<number)
+    {
+        //double cube=Math.Pow(i,3);
+        Console.Write($"{Math.Pow(i,3)}, ");
+        i++;
+    }
+    Console.Write($"{Math.Pow(number,3)}");
+}
+
+int number = GetNNumber("Введите положительное число N:");
+ShowCubeTable(number);
