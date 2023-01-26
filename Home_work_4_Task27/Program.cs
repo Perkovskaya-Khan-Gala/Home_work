2c92a79ba285d6edbbb015cf5ecd6a2bb321d476
Galka <hgalala@mail.ru>
2023-01-26 11:26:43 +0700

    Create folder with home wprk Task 27

---
+++ Home_work_4_Task27/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int GetNumber(string message)
+{
+    int result=0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result)) break;
+        else Console.WriteLine("Введено не число. Попробуйте еще раз");
+    }
+    return result;
+}
+
+int FindSummOfDigits(int number)
+{
+    int result = 0;
+    while (number>0)
+    {
+        result=result+number%10;
+        number=number/10;
+    }
+    return result;
+}
+
+int input_number=GetNumber("Введите число:");
+int sum_result= FindSummOfDigits(input_number);
+Console.WriteLine($"{input_number} -> {sum_result}");
